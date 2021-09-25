42633e4d43a1c08ade85e2fbf547b7c6d042c119
sergiodrm <sergiojg.drm@gmail.com>
2021-09-26 00:35:35 +0200

    asdf

--- premake5.lua
+++ premake5.lua
@@ -14,6 +14,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "VoltEngine/lib/GLFW/include"
 IncludeDir["glad"] = "VoltEngine/lib/glad/include"
 IncludeDir["glm"] = "VoltEngine/lib/glm"
+IncludeDir["spdlog"] = "VoltEngine/lib/spdlog/include"
 
 group "Dependencies"
 	include "VoltEngine/lib/GLFW"
@@ -21,7 +22,107 @@ group "Dependencies"
 
 group "Engine"
 
-include "VoltEngine"
+project "VoltEngine"
+    location "VoltEngine"
+    kind "StaticLib"
+    language "C++"
+
+    cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}-bin")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}-binobj")
+
+    files
+	{
+		"VoltEngine/src/**.h",
+		"VoltEngine/src/**.cpp",
+		"VoltEngine/lib/glm/glm/**.hpp",
+		"VoltEngine/lib/glm/glm/**.inl",
+	}
+
+	includedirs
+	{
+		"VoltEngine/src",
+		"VoltEngine/lib/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.glad}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.spdlog}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"opengl32.lib"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+		"VOLT_PLATFORM_WINDOWS",
+		"GLFW_INCLUDE_NONE", 
+        "VOLT_ENGINE=1"
+		}
+
+
+	filter "configurations:Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		optimize "on"
 
 group "Game"
-include "Sandbox"
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("%{wks.name}/bin/" .. outputdir .. "/%{prj.name}-bin")
+    objdir ("%{wks.name}/bin-int/" .. outputdir .. "/%{prj.name}-binobj")
+
+    files
+    {
+        "Sandbox/src/**.h",
+        "Sandbox/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Sandbox/src",
+        "%{wks.location}/VoltEngine/src",
+        "%{IncludeDir.glm}",
+        "%{IncludeDir.spdlog}"
+    }
+
+    links
+    {
+        "VoltEngine"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+        "VOLT_PLATFORM_WINDOWS",
+        "VOLT_ENGINE=0"
+        }
+
+
+    filter "configurations:Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        optimize "on"
