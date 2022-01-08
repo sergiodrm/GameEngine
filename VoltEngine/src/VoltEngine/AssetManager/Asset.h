ad642699a0f680dc89c315f5d8da2e5b0a7e6dae
sergiodrm <sergiojg.drm@gmail.com>
2022-01-08 19:07:10 +0100

    Virtual destructor in IAsset.

--- VoltEngine/src/VoltEngine/AssetManager/Asset.h
+++ VoltEngine/src/VoltEngine/AssetManager/Asset.h
@@ -6,6 +6,7 @@ namespace Volt
     class IAsset
     {
     public:
+        virtual ~IAsset() = default;
         virtual class IAssetLoader* CreateLoader() = 0;
         virtual bool IsLoaded() const = 0;
     };
