b28d067de8d9eef18f0716da5c930054066ed735
sergiodrm <sergiojg.drm@gmail.com>
2022-01-20 20:53:47 +0100

    Path reference in every asset.

--- VoltEngine/src/VoltEngine/AssetManager/Asset.h
+++ VoltEngine/src/VoltEngine/AssetManager/Asset.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <filesystem>
+
+#include "VoltEngine/Core/Log.h"
 #include "VoltEngine/RTTI/TypeMacros.h"
 
 namespace Volt
@@ -7,9 +10,24 @@ namespace Volt
     class IAsset
     {
     DECLARE_BASE_CLASS(IAsset);
+        friend class CAssetManager;
     public:
         virtual ~IAsset() = default;
         virtual class IAssetLoader* CreateLoader() = 0;
         virtual bool IsLoaded() const = 0;
+
+        virtual std::string GetName() const { return m_path.filename().string(); }
+        virtual std::string GetPath() const { return m_path.string(); }
+
+    private:
+        virtual void SetPath(const std::string& path)
+        {
+            const std::filesystem::path tempPath(path);
+            VOLT_ASSERT(!tempPath.empty() && tempPath.has_extension() && tempPath.has_filename() && tempPath.is_absolute(), "Invalid path format");
+            m_path = tempPath;
+        }
+
+    private:
+        std::filesystem::path m_path;
     };
 }
